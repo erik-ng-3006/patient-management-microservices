2139c951da134993f0d646d6d42c485ec0f6fb6f
erik-ng-3006 <erikng3006@gmail.com>
2025-03-24 20:52:11 +0700

    fix error in KafkaProducer

--- patient-service/src/main/java/com/pm/patient_service/kafka/KafkaProducer.java
+++ patient-service/src/main/java/com/pm/patient_service/kafka/KafkaProducer.java
@@ -1,5 +1,4 @@
 package com.pm.patient_service.kafka;
-
 import com.pm.patient_service.model.Patient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,7 +8,9 @@ import patient.events.PatientEvent;
 
 @Service
 public class KafkaProducer {
-    private static final Logger log = LoggerFactory.getLogger(KafkaProducer.class);
+
+    private static final Logger log = LoggerFactory.getLogger(
+            KafkaProducer.class);
     private final KafkaTemplate<String, byte[]> kafkaTemplate;
 
     public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
@@ -17,20 +18,17 @@ public class KafkaProducer {
     }
 
     public void sendEvent(Patient patient) {
-        // Create a patient event
-        PatientEvent patientEvent = PatientEvent.newBuilder()
+        PatientEvent event = PatientEvent.newBuilder()
                 .setPatientId(patient.getId().toString())
                 .setName(patient.getName())
                 .setEmail(patient.getEmail())
                 .setEventType("PATIENT_CREATED")
                 .build();
 
-        // Send the event using KafkaTemplate
         try {
-            kafkaTemplate.send("patient", patientEvent.toByteArray());
+            kafkaTemplate.send("patient", event.toByteArray());
         } catch (Exception e) {
-            log.error("Error sending PatientCreated event to Kafka: {}", patientEvent);
+            log.error("Error sending PatientCreated event: {}", event);
         }
     }
-
 }
